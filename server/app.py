efa7bce87573b96af04bfe0acb04d4a29644b453
alessandrolandi <117370617+alessandrolandi@users.noreply.github.com>
2023-12-06 14:52:21 -0500

    linted app and user with black

--- server/app.py
+++ server/app.py
@@ -4,16 +4,26 @@ from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
 import os
 from dotenv import load_dotenv
-from flask import Flask, render_template, request, redirect, abort, url_for, make_response, jsonify, session
+from flask import (
+    Flask,
+    render_template,
+    request,
+    redirect,
+    abort,
+    url_for,
+    make_response,
+    jsonify,
+    session,
+)
 from functools import wraps
 
 load_dotenv()
-uri=os.getenv('URI')
+uri = os.getenv("URI")
 
-mongo = MongoClient(uri, server_api=ServerApi('1'))
+mongo = MongoClient(uri, server_api=ServerApi("1"))
 
 try:
-    mongo.admin.command('ping')
+    mongo.admin.command("ping")
     print("Successfully connected to MongoDB!")
 except Exception as e:
     print(e)
@@ -21,53 +31,65 @@ except Exception as e:
 app = Flask(
     __name__, template_folder="../client/templates", static_folder="../client/static"
 )
-app.secret_key = b'\xf6:\\\x0f{\xef(\xb2\\\r\xd4\x9b\xaa\x11\x85'
+
+app.secret_key = os.getenv("SECRET_KEY")
 
 
-#Decorators
+# Decorators
 def login_required(f):
     @wraps(f)
     def wrap(*args, **kwargs):
-        if 'logged_in' in session:
+        if "logged_in" in session:
             return f(*args, **kwargs)
         else:
-            return redirect(url_for('login'))
+            return redirect(url_for("login"))
+
     return wrap
 
-#routes
-@app.route("/", methods=['GET', 'POST'])
+
+# routes
+@app.route("/", methods=["GET", "POST"])
 def login():
-    if request.method == 'POST':
+    if request.method == "POST":
         from models.user import User
+
         return User().login()
     return render_template("login.html")
 
-@app.route("/register", methods=['GET', 'POST'])
+
+@app.route("/register", methods=["GET", "POST"])
 def register():
-    if request.method == 'POST':
+    if request.method == "POST":
         from models.user import User
+
         return User().signup()
     return render_template("registration.html")
+
 
 @app.route("/signout")
 def signout():
     from models.user import User
+
     return User().signout()
+
 
 @app.route("/home/")
 @login_required
 def home():
     return render_template("home.html")
 
+
 @app.route("/chat")
 @login_required
 def chat():
     return render_template("chat.html")
 
+
 @app.route("/profile")
 @login_required
 def profile():
     return render_template("profile.html")
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=3000)
