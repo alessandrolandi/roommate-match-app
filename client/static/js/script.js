d73dfb1df93d4368b7eed68595fbf4542d00fe75
alessandrolandi <117370617+alessandrolandi@users.noreply.github.com>
2023-12-06 14:30:01 -0500

    Created user authentication

---
+++ client/static/js/script.js
@@ -0,0 +1,43 @@
+$("form[name=register-form").submit(function(e) {
+    var $form = $(this);
+    var $error = $form.find(".error");
+    var data = $form.serialize();
+
+    $.ajax({
+        url: "/register",
+        type: "POST",
+        data: data,
+        dataType: "json",
+        success: function(resp){
+            window.location.href = "/home/";
+        },
+        error: function(resp){
+            console.log(resp);
+            $error.text(resp.responseJSON.error).removeClass("error--hidden");
+        }
+    });
+
+    e.preventDefault();
+});
+
+$("form[name=login-form").submit(function(e) {
+    var $form = $(this);
+    var $error = $form.find(".error");
+    var data = $form.serialize();
+
+    $.ajax({
+        url: "/",
+        type: "POST",
+        data: data,
+        dataType: "json",
+        success: function(resp){
+            window.location.href = "/home/";
+        },
+        error: function(resp){
+            console.log(resp);
+            $error.text(resp.responseJSON.error).removeClass("error--hidden");
+        }
+    });
+
+    e.preventDefault();
+});
---
+++ server/models/user.py
@@ -0,0 +1,47 @@
+from flask import Flask, jsonify, request, session, redirect, url_for
+from passlib.hash import pbkdf2_sha256
+from app import mongo
+import uuid
+
+class User:
+    
+    def start_session(self, user):
+        
+        del user['password']
+        session['logged_in'] = True
+        session['user'] = user
+        
+        return jsonify(user), 200
+    
+    def signup(self):
+        
+        user = {
+            "_id": uuid.uuid4().hex,
+            "name": request.form.get('name'),
+            "password": request.form.get('password')
+        }
+        
+        user['password'] = pbkdf2_sha256.encrypt(user['password'])
+        
+        if mongo.db.users.find_one({"name": user['name']}):
+            return jsonify({"error": "Name already in use"}), 400
+        
+        if mongo.db.users.insert_one(user):
+            return self.start_session(user)
+        
+        return jsonify({"error": "Something went wrong"}), 400
+    
+    def signout(self):
+        session.clear()
+        return redirect(url_for('login'))
+    
+    def login(self):
+        
+        user = mongo.db.users.find_one({
+            "name": request.form.get('name')
+        })
+        
+        if user and pbkdf2_sha256.verify(request.form.get('password'), user['password']):
+            return self.start_session(user)
+        
+        return jsonify({ "name": "Invalid login credentials "}), 401
