6efc0b4ac89f3ebe2e1343ddb2826a071828bcff
alessandrolandi <117370617+alessandrolandi@users.noreply.github.com>
2023-12-05 20:42:13 -0500

    Mongodb connection

--- server/app.py
+++ server/app.py
@@ -1,7 +1,22 @@
 """Module providing routing."""
 from flask import Flask, render_template
 import pymongo
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
+import os
+from dotenv import load_dotenv
+from flask import Flask, render_template, request, redirect, abort, url_for, make_response
 
+load_dotenv()
+uri=os.getenv('URI')
+
+mongo = MongoClient(uri, server_api=ServerApi('1'))
+
+try:
+    mongo.admin.command('ping')
+    print("Successfully connected to MongoDB!")
+except Exception as e:
+    print(e)
 
 app = Flask(
     __name__, template_folder="../client/templates", static_folder="../client/static"
@@ -20,4 +35,4 @@ def profile():
     return render_template("profile.html")
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=3000)
