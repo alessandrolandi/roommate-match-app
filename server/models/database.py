59db13619072f33b36f734b06d6b5880b96b7110
Morcupine <mje9832@nyu.edu>
2023-12-15 18:55:28 -0500

    pairing algo basics

--- server/models/database.py
+++ server/models/database.py
@@ -3,11 +3,12 @@ from flask import Flask, request
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
 import array
-
+from app import db
+import pandas as pd
+import numpy as np
 
 class Database:
     db = ""
-    
 
     def __init__(self):
         from app import uri
@@ -41,12 +42,30 @@ class Database:
         
         return matches
 
+    #when a user enters the database or updates his survey, we append all of his matches to the database
 
-    def add_user_matches(self, user, matches):
+
+    def append_user_match(self, user, match):
         self.db.users.find_one_and_update(
-            {"username": user.get("username")}, {'$push': {'matches': matches}}
-            )
+            {"username": user.get("username")}, {'$push': {'matches': match}}
+            )    
         
+    def append_user_match(self, user1, user2):
+        username1 = user1["username"]
+        username2 = user2["username"]
+        user1_survey = db.users.find_one({"username": username1})["survey"] 
+        user2_survey = db.users.find_one({"username": username2})["survey"]  
+        corr = np.corr(user1_survey, user2_survey)
+
+        self.append_user_match(user1, (username2, corr))
+        self.append_user_match(user2, (username1, corr))
+        
+    def append_user(self, user):
+        users = db.users.find({"survey": True})
+
+        for user_i in users:
+            self.append_user_match(user1, user2)
+
     def edit_user_profile(self,user):
         self.db.users.find_one_and_update(
             {"username": user.get("username")} , {'name': request.form.get("name")}
---
+++ server/models/pair.py
@@ -0,0 +1,23 @@
+import pymongo
+import requests
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
+import pandas as pd
+import numpy as np
+import json
+from app import db
+
+users = list(db.users.find({"survey": True}))
+print(users)
+
+survey = dict()
+for user in users:
+    #user.survey should be stored as a dictionary, with keys uniform across all users
+    # corresp. to questions and values corresponding to numerical values for survey questions
+    #insert 
+    survey.append(user["survey"])
+
+
+survey_df = pd.DataFrame(survey)
+corr = survey_df.corr()
+
