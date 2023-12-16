608472c0b164e5f4bcef10df341cb15d48e2ac79
Morcupine <mje9832@nyu.edu>
2023-12-16 18:39:21 -0500

    fixe array != 0 error

--- server/models/pair.py
+++ server/models/pair.py
@@ -17,11 +17,11 @@ class CompareUsers():
 
         #print(f'user_1: {user1_survey}, user_2: {user2_survey}')
 
-        corr = (np.corrcoef(user1_survey, user2_survey) + 1)
+        corr = (np.corrcoef(user1_survey, user2_survey) + 1)[0][1]
         if corr != 0:
             corr /= 2
 
-        corr_str = f"{corr[0][1]:.0%}"
+        corr_str = f"{corr:.0%}"
 
         database.add_user_match(user1, [username2, corr_str]) 
         database.add_user_match(user2, [username1, corr_str])
