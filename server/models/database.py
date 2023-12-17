5b150d591f921c1d4453bdefb54961e74e0d69da
alessandrolandi <117370617+alessandrolandi@users.noreply.github.com>
2023-12-16 21:00:01 -0500

    added datetime for chat fix

--- server/models/database.py
+++ server/models/database.py
@@ -4,6 +4,7 @@ from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
 import array
 import numpy as np
+from datetime import datetime
 
 class DatabaseProxy:
     db = ""
